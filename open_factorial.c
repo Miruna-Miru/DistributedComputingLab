945d2ae3e044cc9267c5a335d9a1aec30fe9bb5a
Miruna-Miru <mirmirunalini@gmail.com>
2025-04-21 21:04:43 +0530

    Openmp factorial and sum utpo n

---
+++ open_factorial.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <omp.h>
+
+int main()  // Always use `int main()` instead of `void main()`
+{
+    int n;
+    printf("Enter n: ");
+    scanf("%d", &n);
+
+    int sum = 1;
+    omp_set_num_threads(3);
+
+    #pragma omp parallel for reduction(*:sum)
+    for(int i = 1; i <= n; i++)
+    {
+        printf("Performing by Thread %d\n", omp_get_thread_num());
+        sum *= i;
+    }
+
+    printf("Final value is: %d\n", sum);
+    return 0;
+}
