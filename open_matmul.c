e573cd79db4533c6d3caa6f8538f524aa7741d3e
Miruna-Miru <mirmirunalini@gmail.com>
2025-04-22 11:20:15 +0530

    Matrix multi using OPENMP

---
+++ open_matmul.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<omp.h>
+
+#define n 3
+
+void main()
+{
+     int a[n][n],b[n][n],c[n][n];
+     int i,j,k;
+     for( i=0;i<n;i++)
+     {
+        for( j=0;j<n;j++)
+        {
+            a[i][j]=1;
+            b[i][j]=1;
+            c[i][j]=0;
+        }
+     }
+
+     #pragma omp parallel for private(i,j,k) shared(a,b,c)
+     for(i=0;i<n;i++)
+     {
+     for(j=0;j<n;j++)
+     {
+     for(k=0;k<n;k++)
+       c[i][j]+=a[i][k]*b[k][j];
+     }
+    }
+   printf("C : ");
+    for( i=0;i<n;i++)
+     {
+        for( j=0;j<n;j++)
+        {
+            printf("%d ",c[i][j]);
+        }
+        printf("\n");
+     }
+
+}
